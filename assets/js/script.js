a3d9c5c171d72b1809ded64c51b33304542a6f30
Madhav Jangid <manumadhavjangid@gmail.com>
2023-10-18 23:37:33 +0530

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,18 @@ let AllSongs = [
         SongFile: 'assets/songs/Blue Eyes.mp3',
         SongName: 'Blue Eyes',
         ImageCover: 'https://imgs.search.brave.com/P58SAyAHN2RtYU5N5BlCjY3BGwiTk6uaJg2y7LcmlvU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLTAwMDA5/NzI0MDM0Mi0ycGFj/ZTctdDUwMHg1MDAu/anBn'
+    },
+    {
+        SongFile: 'assets/songs/Blue Eyes.mp3',
+        SongName: 'Love Dose',
+        ImageCover: 'https://imgs.search.brave.com/7xtFSNT0Dj1Qzy2p8Xb8uNNk0vx_kJjqxe5kGSkOoXA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9U/dm5nWTR1bmpuNC8w/LmpwZw'
+    },
+    {
+        SongFile: 'assets/songs/Blue Eyes.mp3',
+        SongName: 'Kuley Kuley',
+        ImageCover: 'https://imgs.search.brave.com/Yj4UO7PZVg0w7qOu3NEWHOZT_Qx3xB900nfnG8S-_c4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jLnNh/YXZuY2RuLmNvbS82/NjEvS3VsZXktS3Vs/ZXktRnJvbS1Ib25l/eS0zLTAtUHVuamFi/aS0yMDIzLTIwMjMw/OTE0MTEzNTUzLTUw/MHg1MDAuanBn'
     }
 ]
-
 
 
 const PrevButton = document.getElementById('SkipPrevious');
@@ -20,37 +29,47 @@ const NextButton = document.getElementById('SkipNext');
 const BarsGif = document.getElementById('bars');
 const SongName = document.getElementById('SongName');
 const SongImg = document.getElementById('coverimg');
+const TimeBar = document.getElementById('TimeStamp');
+const current = document.getElementById('current');
+const duration = document.getElementById('duration');
 
-var AuidoElement = new Audio(AllSongs[SongIndex].SongFile)
-
-PrevButton.addEventListener('click',function(){
-    SongIndex -= 1;
-    var AuidoElement = new Audio(AllSongs[SongIndex].SongFile);
-})
-
-NextButton.addEventListener('click',function(){
-    SongIndex += 1;
-    var AuidoElement = new Audio(AllSongs[SongIndex].SongFile);
-})
+function ChangeSongs(){
+    
+}
 
 
-PlayButton.addEventListener('click',function(){
-
-    if(AuidoElement.paused || AuidoElement.currentTime < 0){
-        AuidoElement.play();
+var AudioX = new Audio(AllSongs[SongIndex].SongFile);
+function PlaySong(Index){
+    if (AudioX.paused || AudioX.currentTime < 0) {
         PlayButton.classList.remove('bx-play-circle');
         PlayButton.classList.add('bx-pause-circle');
         BarsGif.style.opacity = '1';
-        SongName.innerHTML = AllSongs[SongIndex].SongName;
-        SongImg.src = AllSongs[SongIndex].ImageCover;
-
-    }else{
-        AuidoElement.pause();
+        SongName.innerHTML = AllSongs[Index].SongName;
+        SongImg.src = AllSongs[Index].ImageCover;
+        SongImg.style.display = 'block';
+        duration.innerHTML = ((AudioX.duration)/60);
+        AudioX.play();
+    } else {
+        console.log('Pused')
         PlayButton.classList.remove('bx-pause-circle');
         PlayButton.classList.add('bx-play-circle');
         BarsGif.style.opacity = '0';
         SongName.innerHTML = '';
         SongImg.src = '';
-
+        duration.innerHTML = '';
+        current.innerHTML = '';
+        SongImg.style.display = 'none';
+        AudioX.pause();
     }
+}
+
+
+AudioX.addEventListener('timeupdate', function() {  
+    var progress = ((AudioX.currentTime/AudioX.duration)*100)
+    TimeBar.value = progress;
+    current.innerHTML = AudioX.currentTime;
+});
+
+TimeBar.addEventListener('change',function(){
+    AudioX.currentTime = (TimeBar.value*AudioX.duration)/100;
 })
