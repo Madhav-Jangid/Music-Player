e4232136abf06687ca77a834ac9920c119db3b2f
Madhav Jangid <manumadhavjangid@gmail.com>
2023-10-21 14:54:35 +0530

    x

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,80 @@
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 32) {
+        console.log("Spacebar was pressed.");
+        PlaySong(0);
+    }
+});
 var SongIndex = 0;
 let AllSongs = [
     {
-        SongFile: 'assets/songs/One Thousand Miles.mp3',
+        SongFile: 'assets/songs/YOYO/One Thousand Miles.mp3',
         SongName: 'One Thusand Miles',
-        ImageCover: 'https://imgs.search.brave.com/8j0L7nod2E1AH0B7F0dPqNiTdSAEEtmbYGhAvz27sAs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jLnNh/YXZuY2RuLmNvbS8x/NzMvWW8tWW8tSG9u/ZXktU2luZ2gtSGlu/ZGktMjAxOS0yMDE5/MDMzMTAxMjczNS01/MDB4NTAwLmpwZw'
+        ImageCover: 'https://imgs.search.brave.com/8j0L7nod2E1AH0B7F0dPqNiTdSAEEtmbYGhAvz27sAs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jLnNh/YXZuY2RuLmNvbS8x/NzMvWW8tWW8tSG9u/ZXktU2luZ2gtSGlu/ZGktMjAxOS0yMDE5/MDMzMTAxMjczNS01/MDB4NTAwLmpwZw',
+        details: '<b>Singer:</b> YoYo Honey Singh<br><b>Release Date:</b> 0/0/0<br><b>Album:</b>Album Name',
+        duration: '4:43'
+
+
     },
     {
-        SongFile: 'assets/songs/Blue Eyes.mp3',
+        SongFile: 'assets/songs/YOYO/Blue Eyes.mp3',
         SongName: 'Blue Eyes',
-        ImageCover: 'https://imgs.search.brave.com/P58SAyAHN2RtYU5N5BlCjY3BGwiTk6uaJg2y7LcmlvU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLTAwMDA5/NzI0MDM0Mi0ycGFj/ZTctdDUwMHg1MDAu/anBn'
+        ImageCover: 'https://imgs.search.brave.com/P58SAyAHN2RtYU5N5BlCjY3BGwiTk6uaJg2y7LcmlvU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLTAwMDA5/NzI0MDM0Mi0ycGFj/ZTctdDUwMHg1MDAu/anBn',
+        details: '<b>Singer:</b> YoYo Honey Singh<br><b>Release Date:</b> 0/0/0<br><b>Album:</b>Album Name',
+        duration: '3:40'
+
     },
     {
-        SongFile: 'assets/songs/Love Dose.mp3',
+        SongFile: 'assets/songs/YOYO/Love Dose.mp3',
         SongName: 'Love Dose',
-        ImageCover: 'https://imgs.search.brave.com/7xtFSNT0Dj1Qzy2p8Xb8uNNk0vx_kJjqxe5kGSkOoXA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9U/dm5nWTR1bmpuNC8w/LmpwZw'
+        ImageCover: 'https://imgs.search.brave.com/7xtFSNT0Dj1Qzy2p8Xb8uNNk0vx_kJjqxe5kGSkOoXA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9U/dm5nWTR1bmpuNC8w/LmpwZw',
+        details: '<b>Singer:</b> YoYo Honey Singh<br><b>Release Date:</b> 0/0/0<br><b>Album:</b>Album Name',
+        duration: '3:44'
+
     },
     {
-        SongFile: 'assets/songs/Kuley Kuley.mp3',
+        SongFile: 'assets/songs/YOYO/Kuley Kuley.mp3',
         SongName: 'Kuley Kuley',
-        ImageCover: 'https://imgs.search.brave.com/Yj4UO7PZVg0w7qOu3NEWHOZT_Qx3xB900nfnG8S-_c4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jLnNh/YXZuY2RuLmNvbS82/NjEvS3VsZXktS3Vs/ZXktRnJvbS1Ib25l/eS0zLTAtUHVuamFi/aS0yMDIzLTIwMjMw/OTE0MTEzNTUzLTUw/MHg1MDAuanBn'
+        ImageCover: 'https://imgs.search.brave.com/Yj4UO7PZVg0w7qOu3NEWHOZT_Qx3xB900nfnG8S-_c4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jLnNh/YXZuY2RuLmNvbS82/NjEvS3VsZXktS3Vs/ZXktRnJvbS1Ib25l/eS0zLTAtUHVuamFi/aS0yMDIzLTIwMjMw/OTE0MTEzNTUzLTUw/MHg1MDAuanBn',
+        details: '<b>Singer:</b> YoYo Honey Singh<br><b>Release Date:</b> 0/0/0<br><b>Album:</b>Album Name',
+        duration: '3:30'
+
     }
 ]
 
+
+var PlaylistList = ['yoyo','arijit','bollyWood'];
+var ListIndex = 0;
+
+var Midsection = document.querySelectorAll('.mid-section');
+
+
+
+function NavigatePlaylist(dir) {
+    if (dir == 'left') {
+        if (ListIndex == 0) {
+            ListIndex = AllSongs.length - 1;
+            
+        } else {
+            ListIndex -= 1;
+        }
+        
+        Midsection.forEach((item) => {
+            item.style.display = 'none'
+        })
+        document.getElementById(PlaylistList[ListIndex]).style.display = block;
+    }
+    else if (dir == 'right') {
+        if (ListIndex == AllSongs.length - 1) {
+            ListIndex = 0
+        } else {
+            ListIndex += 1;
+        }
+        Midsection.forEach((item) => {
+            item.style.display = 'none'
+        })
+        document.getElementById(PlaylistList[ListIndex]).style.display = block;
+    }
+}
 
 const PrevButton = document.getElementById('SkipPrevious');
 const PlayButton = document.getElementById('play');
@@ -29,37 +82,102 @@ const NextButton = document.getElementById('SkipNext');
 const BarsGif = document.getElementById('bars');
 const SongName = document.getElementById('SongName');
 const SongImg = document.getElementById('coverimg');
-const TimeBar = document.getElementById('TimeStamp');
-const current = document.getElementById('current');
 const duration = document.getElementById('duration');
 const smallPlay = document.querySelectorAll('.song-play');
+const detailsRightNav = document.getElementById('detailsRightNav');
 
 var ButtonArray = [];
 smallPlay.forEach((item) => {
     ButtonArray.push(item);
 })
 
-var AudioX = new Audio(AllSongs[SongIndex].SongFile);
 
-function ChangeSongs(Index) {
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    const formattedTime = `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+    return ('-' + formattedTime);
+}
+
+var AudioX = new Audio(AllSongs[SongIndex].SongFile);
+const TimeBar = document.getElementById('TimeStamp');
+const current = document.getElementById('current');
+const Volume = document.getElementById('volumeRange')
+
+function ChangeSongs(Index,Is) {
+    if (Is) {
+        AudioX.pause();
+        setTimeout(() => {
+            AudioX.play()
+        }, 200);
+    }
     AudioX.pause();
     AudioX = new Audio(AllSongs[Index].SongFile);
     if (AudioX.paused || AudioX.currentTime < 0) {
+        AudioX.play();
         AudioX.addEventListener('timeupdate', function () {
             var progress = ((AudioX.currentTime / AudioX.duration) * 100)
             TimeBar.value = progress;
-            current.innerHTML = AudioX.currentTime;
+            current.innerHTML = formatTime(AudioX.currentTime);
+            Volume.addEventListener('change', function () {
+                AudioX.volume = Volume.value;
+            })
+
+            if (AudioX.currentTime == AudioX.duration) {
+                Index += 1;
+                if (Index < AllSongs.length) {
+                    PlaySong(Index);
+                } else {
+                    PlaySong(0);
+                }
+            }
         });
-        TimeBar.addEventListener('change', function () {
-            AudioX.currentTime = (TimeBar.value * AudioX.duration) / 100;
-        })
-        AudioX.play();
-        // PlaySong(Index);
+        // TImebarUpdate();
     } else {
         AudioX.pause();
-        // PlaySong(Index);
     }
 }
+
+
+var VolumeButton = document.getElementById('volIcon');
+var volumeRange = document.getElementById('volumeRange')
+VolumeButton.addEventListener('click', function () {
+    if (volumeRange.value > 0) {
+        volumeRange.value = 0;
+        AudioX.volume = 0;
+        VolumeButton.classList.remove('bx-volume-low');
+        VolumeButton.classList.add('bx-volume-mute');
+    } else {
+        volumeRange.value = 1;
+        AudioX.volume = 1;
+        VolumeButton.classList.remove('bx-volume-mute');
+        VolumeButton.classList.add('bx-volume-full');
+    }
+})
+volumeRange.addEventListener('change', function () {
+    console.log(parseFloat(volumeRange.value) * 100)
+    if (volumeRange.value <= 0.5) {
+        VolumeButton.classList.remove('bx-volume-mute');
+        VolumeButton.classList.remove('bx-volume-full');
+        VolumeButton.classList.add('bx-volume-low');
+    } else if (Math.abs(volumeRange.value - 0) < 0.001) {
+        VolumeButton.classList.remove('bx-volume-mute');
+        VolumeButton.classList.remove('bx-volume-low');
+        VolumeButton.classList.add('bx-volume-mute');
+        console.log('muted')
+    } else {
+        VolumeButton.classList.remove('bx-volume-mute');
+        VolumeButton.classList.remove('bx-volume-low');
+        VolumeButton.classList.add('bx-volume-full');
+    }
+
+})
+
+
+TimeBar.addEventListener('change', function () {
+    AudioX.currentTime = (TimeBar.value * AudioX.duration) / 100;
+})
+
 
 const closeRight = document.getElementById('closeRightNav');
 const RightNav = document.getElementById('right-nav');
@@ -68,7 +186,7 @@ const CenterSlide = document.getElementById('center-ele');
 const rightname = document.querySelectorAll('.current-song-name');
 const rightimg = document.querySelectorAll('.current-song-img');
 
-function rightnav(Index){
+function rightnav(Index) {
     rightname.forEach((name) => {
         name.innerHTML = AllSongs[Index].SongName;
     })
@@ -79,27 +197,36 @@ function rightnav(Index){
 
 }
 
+const BottomNav = document.getElementById('BottomNav');
 
-function PlaySong(Index) {
+
+function PlaySong(Index, Is) {
+    SongIndex = Index;
+    let Div = document.getElementById(Index);
+    Div.style.backgroundColor = '#333';
+    Div.style.border = '2px solid rgb(0, 115, 255)'
+
     if (AudioX.paused || AudioX.currentTime < 0) {
+        ChangeSongs(Index, Is);
         PlayButton.classList.remove('bx-play-circle');
         PlayButton.classList.add('bx-pause-circle');
+        PlayButton.style.color = 'rgb(35, 232, 55)';
         RightNav.style.display = 'block';
         CenterSlide.style.width = '50vw';
+        BottomNav.style.transform = 'translateY(0rem)';
         rightnav(Index);
-        BarsGif.style.opacity = '1';
+        // BarsGif.style.opacity = '1';
         SongName.innerHTML = AllSongs[Index].SongName;
         SongImg.src = AllSongs[Index].ImageCover;
+        detailsRightNav.innerHTML = AllSongs[Index].details;
         SongImg.style.display = 'block';
-        duration.innerHTML = ((AudioX.duration) / 60);
-        ChangeSongs(Index);
+        duration.innerHTML = AllSongs[Index].duration;
         ButtonArray.forEach((i) => {
             if (Index == ButtonArray.indexOf(i)) {
                 // null
             } else {
                 i.classList.remove('bx-pause-circle');
                 i.classList.add('bx-play-circle');
-
             }
         })
 
@@ -107,13 +234,13 @@ function PlaySong(Index) {
         console.log('Pused')
         PlayButton.classList.remove('bx-pause-circle');
         PlayButton.classList.add('bx-play-circle');
-        RightNav.style.display = 'none';
-        CenterSlide.style.width = '80vw';
-        BarsGif.style.opacity = '0';
+        PlayButton.style.color = '#fff';
+        // RightNav.style.display = 'none';
+        // CenterSlide.style.width = '80vw';
+        // BottomNav.style.transform = 'translateY(7rem)';
+        // BarsGif.style.opacity = '0';
         SongName.innerHTML = '';
         SongImg.src = '';
-        duration.innerHTML = '';
-        current.innerHTML = '';
         SongImg.style.display = 'none';
         AudioX.pause();
     }
@@ -121,20 +248,29 @@ function PlaySong(Index) {
 
 function NavigateSong(dir) {
     if (dir == 'left') {
-        SongIndex -= 1;
-        ChangeSongs(SongIndex);
-        rightnav(SongIndex);
+        if (SongIndex == 0) {
+            SongIndex = AllSongs.length - 1;
+
+        } else {
+            SongIndex -= 1;
+        }
+        PlaySong(SongIndex, true);
+        rightnav(SongIndex, true);
     }
     else if (dir == 'right') {
-        SongIndex += 1;
-        ChangeSongs(SongIndex);
-        rightnav(SongIndex);
+        if (SongIndex == AllSongs.length - 1) {
+            SongIndex = 0
+        } else {
+            SongIndex += 1;
+        }
+        ChangeSongs(SongIndex, true);
+        rightnav(SongIndex, true);
     }
 }
 
 
-
-function DisplayArtist(id){
+function DisplayArtist(id, index) {
+    DisplayPlaylist(index);
     const section = document.querySelectorAll('.mid-section');
     section.forEach((item) => {
         item.style.display = 'none';
@@ -142,10 +278,25 @@ function DisplayArtist(id){
     document.getElementById(id).style.display = 'block';
 }
 
+const section = document.querySelectorAll('.playlist');
+function DisplayPlaylist(id) {
+    section.forEach((item) => {
+        item.classList.remove('active');
+    })
+    document.getElementById(id).classList.add('active')
+}
 
 
 
-closeRight.addEventListener('click',function(){
+
+
+closeRight.addEventListener('click', function () {
     RightNav.style.display = 'none';
-    CenterSlide.style.width = '80vw'
+    CenterSlide.style.width = '80vw';
 })
+
+
+function PlayPause() {
+    PlaySong(SongIndex);
+}
+
